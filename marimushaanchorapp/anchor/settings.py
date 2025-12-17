2e81dff17ea5fa7df80934623dd29debf181d8ce
macmhonyera <macmhonyera@gmail.com>
2025-12-17 13:01:43 +0200

    my changes

--- marimushaanchorapp/anchor/settings.py
+++ marimushaanchorapp/anchor/settings.py
@@ -295,11 +295,11 @@ WSGI_APPLICATION = 'marimushaanchorapp.anchor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER',),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', "5432"),
+        'NAME':'australiaanchor',
+        'USER':'anchor_australia_user',
+        'PASSWORD':'Letmein99x!',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
